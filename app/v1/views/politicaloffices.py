96722060aec400ab1f2e7bfc420a5c735a27a161
Christine Wanjiru <christine.gichuki@strathmore.edu>
2019-02-06 10:00:48 +0300

    [finishes #163682549] view specific office route

--- app/v1/views/politicaloffices.py
+++ app/v1/views/politicaloffices.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, make_response, jsonify, request, Blueprint
+from flask import Flask, make_response, jsonify, request, Blueprint, abort
 from .politicalmain import api
 
 app = Flask(__name__)
@@ -18,13 +18,13 @@ def add_politicaloffices():
     """
     if request.method == "POST":
         data = request.get_json()
-        type = data ['type']
+        office_type = data ['office_type']
         name = data ['name']
         
 
         new_politicaloffice = { 
                 "id": generate_id(politicaloffices_list),
-                "type" : type ,
+                "type" : office_type ,
                 "name" : name 
                 
                 }
@@ -35,7 +35,7 @@ def add_politicaloffices():
             "Message": "New Political Office Created",
             "office name": new_politicaloffice['name'],
             "office id": new_politicaloffice['id'],
-    }), 201)
+            }), 201)
 
     elif request.method == "GET":
         #Get all political offices - GET request
@@ -43,4 +43,19 @@ def add_politicaloffices():
                 "offices": politicaloffices_list,
                 "status": "Ok"
             }), 200)
+
+@api.route('/politicaloffices/<int:id>', methods = ["GET"])
+def specific_politicaloffice(id):
+    #View specific political party - GET request
+    new_politicaloffice = [new_politicaloffice for new_politicaloffice in politicaloffices_list if new_politicaloffice['id'] == id]
+    
+    if len(new_politicaloffice) == 0:
+        abort(404, 'Office does not exist')
+
+    if request.method == 'GET':
+        return make_response(jsonify({
+                    "parties": politicaloffices_list,
+                    "status": "Ok"
+                }), 200)
+
 
