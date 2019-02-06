53d9ecf4f553411d63e05202908ca36ee03a6925
Christine Wanjiru <christine.gichuki@strathmore.edu>
2019-02-06 09:02:52 +0300

    [finishes #163675143] adds view specific party route

--- app/v1/views/politicalparties.py
+++ app/v1/views/politicalparties.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, make_response, jsonify, request, abort
 from .politicalmain import api
 
 app = Flask(__name__)
@@ -35,6 +35,7 @@ def add_politicalparties():
                 }
         
         politicalparties_list.append(new_politicalparty)
+        
         return make_response(jsonify({
             "Message": "New Political Party Created",
             "party name": new_politicalparty['name'],
@@ -47,3 +48,21 @@ def add_politicalparties():
                 "parties": politicalparties_list,
                 "status": "Ok"
             }), 200)
+
+@api.route('/politicalparties/<int:id>', methods = ["GET"])
+def specific_politicalparty(id):
+    #View specific political party - GET request
+    new_politicalparty = [new_politicalparty for new_politicalparty in politicalparties_list if new_politicalparty['id'] == id]
+    
+    if len(new_politicalparty) == 0:
+        abort(404, 'Party does not exist')
+
+    if request.method == 'GET':
+        return make_response(jsonify({
+                    "parties": politicalparties_list,
+                    "status": "Ok"
+                }), 200)
+
+
+            
+
