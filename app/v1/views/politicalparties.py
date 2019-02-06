273da110c642d14d77a5c4de2fa5003ba3ddc82d
Christine Wanjiru <christine.gichuki@strathmore.edu>
2019-02-06 05:48:29 +0300

    [finishes #163675105] adds view all parties route

--- app/v1/views/politicalparties.py
+++ app/v1/views/politicalparties.py
@@ -11,30 +11,39 @@ def generate_id(list):
 
     return len(list) + 1
 
-@api.route('/politicalparties', methods = ["POST"])
+@api.route('/politicalparties', methods = ["POST","GET"])
 def add_politicalparties():
     """
         Create a new political party  - POST request
     """
-    data = request.get_json()
-    name = data ['name']
-    abbreviation = data ['abbreviation']
-    members = data ['members']
-    headquarters = data ['headquarters']
-    chairperson = data ['chairperson']
+    if request.method == "POST":
 
-    new_politicalparty = { 
-            "id": generate_id(politicalparties_list),
-            "name" : name ,
-            "abbreviation" : abbreviation ,
-            "members" : members ,
-            "headquarters": headquarters,
-            "chairperson": chairperson
-            }
-    
-    politicalparties_list.append(new_politicalparty)
-    return make_response(jsonify({
-        "Message": "New Political Party Created",
-        "party name": new_politicalparty['name'],
-        "party id": new_politicalparty['id'],
- }), 201)
+        data = request.get_json()
+        name = data ['name']
+        abbreviation = data ['abbreviation']
+        members = data ['members']
+        headquarters = data ['headquarters']
+        chairperson = data ['chairperson']
+
+        new_politicalparty = { 
+                "id": generate_id(politicalparties_list),
+                "name" : name ,
+                "abbreviation" : abbreviation ,
+                "members" : members ,
+                "headquarters": headquarters,
+                "chairperson": chairperson
+                }
+        
+        politicalparties_list.append(new_politicalparty)
+        return make_response(jsonify({
+            "Message": "New Political Party Created",
+            "party name": new_politicalparty['name'],
+            "party id": new_politicalparty['id'],
+         }), 201)
+
+
+    elif request.method == "GET":
+        return make_response(jsonify({
+                "parties": politicalparties_list,
+                "status": "Ok"
+            }), 200)
