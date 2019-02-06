c58e255c76a9aa80e57bb77d1e643c26fc2adff2
Christine Wanjiru <christine.gichuki@strathmore.edu>
2019-02-06 05:15:51 +0300

    [finishes #163748126] tests view all parties route

--- app/v1/views/politicaloffices.py
+++ app/v1/views/politicaloffices.py
@@ -11,26 +11,32 @@ def generate_id(list):
 
     return len(list) + 1
 
-@api.route('/politicaloffices', methods = ["POST"])
+@api.route('/politicaloffices', methods = ["POST", "GET"])
 def add_politicaloffices():
     """
         Create a new political office  - POST request
     """
-    data = request.get_json()
-    type = data ['type']
-    name = data ['name']
-    
+    if request.method == "POST":
+        data = request.get_json()
+        type = data ['type']
+        name = data ['name']
+        
 
-    new_politicaloffice = { 
-            "id": generate_id(politicaloffices_list),
-            "type" : type ,
-            "name" : name 
-            
-            }
-    
-    politicaloffices_list.append(new_politicaloffice)
-    return make_response(jsonify({
-        "Message": "New Political Office Created",
-        "office name": new_politicaloffice['name'],
-        "office id": new_politicaloffice['id'],
- }), 201)
+        new_politicaloffice = { 
+                "id": generate_id(politicaloffices_list),
+                "type" : type ,
+                "name" : name 
+                
+                }
+        
+        politicaloffices_list.append(new_politicaloffice)
+        return make_response(jsonify({
+            "Message": "New Political Office Created",
+            "office name": new_politicaloffice['name'],
+            "office id": new_politicaloffice['id'],
+    }), 201)
+
+    elif request.methods == "GET":
+        return make_response(jsonify(politicaloffices_list), 200)
+
+
