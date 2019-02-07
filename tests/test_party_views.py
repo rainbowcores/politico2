b024c35cc7d53a41556ceae46cde07816f2fbdfe
Christine Wanjiru <christine.gichuki@strathmore.edu>
2019-02-07 04:22:24 +0300

    [finishes #163751213] tests edit specific party name route

--- tests/test_party_views.py
+++ tests/test_party_views.py
@@ -40,6 +40,12 @@ class PoliticalPartiesTestCase(unittest.TestCase):
         
         response = self.client.delete('/api/v1/politicalparties/1', json=self.politicalparty)
         self.assertEqual(response.status_code, 200)
+
+    def test_edit_specific_party(self):
+        
+        self.client.post('/api/v1/politicalparties', json=self.politicalparty)
+        response = self.client.patch('/api/v1/politicalparties/1/Orange')
+        self.assertEqual(response.status_code, 200)
         
 
 
