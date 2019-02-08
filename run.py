a5205d85f2a7d6b32e55efc3b3efe08cdebcaa09
Christine Wanjiru <christine.gichuki@strathmore.edu>
2019-02-08 20:56:57 +0300

    he

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('APP_SETTINGS') # config_name = "development"
-app = create_app(config_name)
+#config_name = os.getenv('APP_SETTINGS') # config_name = "development"
+app = create_app('development')
 
 
 
