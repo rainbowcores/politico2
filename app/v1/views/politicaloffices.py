f8275f9480cc4bc1ab3ceee8ed9e5d0ac617912d
Christine Wanjiru <christine.gichuki@strathmore.edu>
2019-02-16 12:17:05 +0300

    [Finishes #164020371] create edit and delete office routes

--- app/v1/views/politicaloffices.py
+++ app/v1/views/politicaloffices.py
@@ -6,42 +6,65 @@ from ..models.officesmodel import OfficeModel, politicaloffices_list
 app = Flask(__name__)
 
 
-
-
-
-@api.route('/politicaloffices', methods = ["POST", "GET"])
+@api.route('/politicaloffices', methods=["POST", "GET"])
 def add_politicaloffices():
-   
-    #Create a new political office  - POST request
-    
     if request.method == "POST":
         data = request.get_json()
         try:
-            name = data ['name']
-            office_type = data ['office_type']
+            name = data['name']
+            office_type = data['office_type']
             if not office_type.isalpha() or not name.isalpha():
-                return response (400, "Office type and name should be text", [])
-            
-            office= OfficeModel.get_specific_office_name(data ['name'])
+                return response(400, "Office type and name should be text", [])
+
+            office = OfficeModel.get_specific_office_name(data['name'])
             if not office:
-                new_politicaloffice= OfficeModel(name, office_type)
+                new_politicaloffice = OfficeModel(name, office_type)
                 politicaloffices_list.append(new_politicaloffice)
-                return response (201, "New office was created", [new_politicaloffice.to_json()])
+                return response(201, "New office was created", [new_politicaloffice.to_json()])
             else:
-                return response(409, "The party exists", [])
+                return response(409, "The office exists", [])
         except:
-            return response (400, "Please fill in all the fields, office type and name ", [])
-        
-        
+            return response(400, "Please fill in all the fields, office type and name ", [])
+
     elif request.method == "GET":
-        #Get all political offices - GET request
         return response(200, "", [office.to_json() for office in politicaloffices_list])
 
-@api.route('/politicaloffices/<int:office_id>', methods = ["GET"])
+
+@api.route('/politicaloffices/<int:office_id>', methods=["GET", "DELETE"])
 def specific_politicaloffice(office_id):
-    #View specific political office - GET request
     global politicaloffices_list
+    if request.method == "GET":
+        office = OfficeModel.get_specific_office(office_id)
+        if office:
+                return response(200, "", [office.to_json()])
+        else:
+            return response(404, "office does not exist", [])
+    elif request.method == "DELETE":
+        office = OfficeModel.get_specific_office(office_id)
+        if office:
+            politicaloffices_list.remove(office)
+            return response(200, "deleted successfully", [office.to_json() for office in politicaloffices_list])
+        else:
+            return response(404, "office does not exist", [])
 
-    office= OfficeModel.get_specific_office(office_id)
-    return response(200, "", [office.to_json()])
 
+@api.route('/politicaloffices/<int:office_id>', methods=["PATCH"])
+def edit_office(office_id):
+        data = request.get_json()
+        office = OfficeModel.get_specific_office_name(data['name'])
+        if not office:
+            try:
+                name = data['name']
+                office_type = data['office_type']
+
+                office = OfficeModel.patch_office_name(office_id, name, office_type)
+                if office:
+                    office.name = name
+                    office.office_type = office_type
+                    return response(200, "office name changes", [office.to_json()])
+                else:
+                    return response(404, "office does not exist", [])
+            except KeyError:
+                return response(202, "Key error occured, please enter all the fields", [])
+        else:
+            return response(409, "The office name exists", [])
--- app/v1/views/politicalparties.py
+++ app/v1/views/politicalparties.py
@@ -5,15 +5,9 @@ from ..models.partiesmodel import PartyModel, politicalparties_list
 app = Flask(__name__)
 
 
-
-
-
-
-
-@api.route('/politicalparties', methods = ["POST","GET"])
+@api.route('/politicalparties', methods=["POST", "GET"])
 def add_politicalparties():
-    #Create a new political party  - POST request
-    
+
     if request.method == "POST":
         data = request.get_json()
         try:
@@ -21,68 +15,62 @@ def add_politicalparties():
             logoUrl = data['logoUrl']
             hqAddress = data['hqAddress']
             if not name.isalpha() or not logoUrl.isalpha() or not hqAddress.isalpha():
-                return response (400, "Please fill in all the fields as name, logoUrl and hqAddress as text",[])
-            party= PartyModel.get_specific_party_name(data['name']) 
+                return response(400, "All the fields name, logoUrl and hqAddress as text", [])
+            party = PartyModel.get_specific_party_name(data['name'])
             if not party:
-                new_politicalparty= PartyModel(name, logoUrl, hqAddress)
+                new_politicalparty = PartyModel(name, logoUrl, hqAddress)
                 politicalparties_list.append(new_politicalparty)
-                return response (201, "New party was created", [new_politicalparty.to_json()])
+                return response(201, "New party was created", [new_politicalparty.to_json()])
             else:
                 return response(409, "The party exists", [])
         except:
-            return response (400, "Please fill in all the fields, name, logoUrl and hqAddress ", [])
+            return response(400, "Please fill in all the fields, name, logoUrl and hqAddress ", [])
     elif request.method == "GET":
-    
+
         return response(200, "", [party.to_json() for party in politicalparties_list])
     else:
 
         return response(405, "The method used is not allowed", [])
 
-@api.route('/politicalparties/<int:party_id>', methods = ["GET", "DELETE"])
-def specific_politicalparty(party_id):
-    #View specific political party - GET request
-    global politicalparties_list
-    
 
+@api.route('/politicalparties/<int:party_id>', methods=["GET", "DELETE"])
+def specific_politicalparty(party_id):
+
+    global politicalparties_list
     if request.method == "GET":
-        party= PartyModel.get_specific_party(party_id)
+        party = PartyModel.get_specific_party(party_id)
         if party:
             return response(200, "", [party.to_json()])
         else:
-            return response(404, "party does not exist", []) 
-        
-    elif request.method== "DELETE":
-        party= PartyModel.get_specific_party(party_id) 
+            return response(404, "party does not exist", [])
+    elif request.method == "DELETE":
+        party = PartyModel.get_specific_party(party_id)
         if party:
-            politicalparties_list.remove(party)             
-            return response(204, "deleted successfully", [party.to_json() for party in politicalparties_list] )
-
-            
+            politicalparties_list.remove(party)
+            return response(200, "deleted successfully", [party.to_json() for party in politicalparties_list])
         else:
-            return response(404, "party does not exist", [])   
-    
+            return response(404, "party does not exist", [])
 
 
-
-@api.route('/politicalparties/<int:party_id>', methods = ["PATCH"])
+@api.route('/politicalparties/<int:party_id>', methods=["PATCH"])
 def edit_party_name(party_id):
         data = request.get_json()
-        party= PartyModel.get_specific_party_name(data['name']) 
+        party = PartyModel.get_specific_party_name(data['name'])
         if not party:
             try:
                 name = data['name']
                 logoUrl = data['logoUrl']
                 hqAddress = data['hqAddress']
 
-                party = PartyModel.patch_party_name(party_id, name,logoUrl, hqAddress)
+                party = PartyModel.patch_party_name(party_id, name, logoUrl, hqAddress)
                 if party:
-                    party.name= name
-                    party.logoUrl= logoUrl
-                    party.hqAddress= hqAddress
-                    return response (200, "party name changes", [party.to_json()])
+                    party.name = name
+                    party.logoUrl = logoUrl
+                    party.hqAddress = hqAddress
+                    return response(200, "party name changes", [party.to_json()])
                 else:
                     return response(404, "party does not exist", [])
             except KeyError:
-                return response (202, "Key error occured, please enter all the fields", [])
+                return response(202, "Key error occured, please enter all the fields", [])
         else:
             return response(409, "The party name exists", [])
